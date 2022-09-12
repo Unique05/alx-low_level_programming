cd957b1b57ea3a0c7b715a50b7e22b97d7cb9717
Unique05 <perfectok05@gmail.com>
2022-09-12 14:18:34 -0700

    s

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Function outputs with proper grammar, but the outcome is a piece of art,*/
+
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+;
+return 0;
+}
