fa2bc2696356887fe05f491b428c3a464c683816
Unique05 <perfectok05@gmail.com>
2022-09-12 14:58:38 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ long int c;
 long long int d;
 float e;
 
+
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
