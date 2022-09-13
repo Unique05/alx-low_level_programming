9ba0a4094494e9f06333bdbff33dd28c142454c7
Unique05 <perfectok05@gmail.com>
2022-09-13 15:19:09 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits
+ * Description: function prints sing digits
+ * in base ten on a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+int c;
+	for (c = 0; c < 10; c++)
+		putchar(c + '0');
+	printf("\n");
+	return (0);
+}
