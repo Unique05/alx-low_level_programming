886f9c95284098546904b6d3fc74c02f51a7a79c
Unique05 <perfectok05@gmail.com>
2022-09-13 15:07:20 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits
+ * Description: Prints all single
+ * numbers in base ten
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		printf("%d", c);
+	printf("\n");
+	return (0);
+}
