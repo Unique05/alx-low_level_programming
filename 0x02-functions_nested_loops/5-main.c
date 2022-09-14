8625bbab1615175b32bc3f425fd66cec645545c6
Unique05 <perfectok05@gmail.com>
2022-09-14 16:23:00 -0700

    s

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -26,13 +26,13 @@ int main(void)
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
-       	_putchar('\n');
+	_putchar('\n');
 
 	r = print_sign(-1);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-	
+
 	return (0);
 }
